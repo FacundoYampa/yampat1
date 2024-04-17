caf7c8607f5903ad323013e4cb57c4d9d253d34e
FacundoYampa <facundoyampa@gmail.com>
2024-04-17 20:53:34 -0300

    clase 17/4

---
+++ clases/clase5/form_clase5.js
@@ -0,0 +1,8 @@
+function tests(){
+    const nombre = document.getElementById("Nombre").value;
+    const apellido = document.getElementById("Apellido").value;
+    const email = document.getElementById("Email").value;
+    const password = document.getElementById("Password").value;
+    const repPassword = document.getElementById("RepPassword").value;
+    const fecha = document.getElementById("FechaNaciemiento").value;
+}
---
+++ js/form_tarea.js
@@ -0,0 +1,4 @@
+function validaciones(){
+    const email = document.getElementById("email").value;
+    const pass = document.getElementById("password").value;
+}
